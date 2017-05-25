1bc691868be90663d59175649433cbc132c00492
Trevor Green <Trevor@mailagc.com>
2017-05-25 09:50:03 -0700

    Removing duplicate io.on
    I removed the io.sockets.on('connection') as it appeared to be a duplicate. I included the socket.on('send message') handler in the other io.on connection. I don't think the first one would work anyway, but I didn't test it for an error.

--- server.js
+++ server.js
@@ -12,27 +12,22 @@ app.get('/', function(req, res) {
     app.use('/public', express.static(path.join(__dirname, 'public')))
 });
 
-io.sockets.on('connection', function(socket) {
-    socket.on('send message', function(data) {
-        io.sockets.emit('new message', data);
-    	});
-	});
-});
-
-
 io.on('connection', function(socket) {
-            socket.emit('news', {
-                hello: 'world'
-            });
-            socket.on('my other event', function(data) {
-                console.log(data);
-            });
-            socket.on('news', function(msg) {
-                console.log('message: ' + msg);
+	socket.emit('news', {
+        	hello: 'world'
+        });
+        socket.on('send message', function(data) {
+        	io.sockets.emit('new message', data);
+    	});
+	socket.on('my other event', function(data) {
+        	console.log(data);
+        });
+        socket.on('news', function(msg) {
+        	console.log('message: ' + msg);
                 io.emit('news', msg);
-            });
-            io.on('connection', function(socket) {
-                socket.on('chat message', function(msg) {
-                    io.emit('chat message', msg);
-                });
-            });
+        });
+        io.on('connection', function(socket) {
+        	socket.on('chat message', function(msg) {
+                io.emit('chat message', msg);
+        	});
+});
